f4b958f2c366f6c4907af40130e0fa6f29a17061
Bee <vuhuyhoang310199@gmail.com>
2026-01-03 20:38:59 +0700

    Update category DDD

--- app-modules/categories/src/Domain/Interfaces/CategoryRepositoryInterface.php
+++ app-modules/categories/src/Domain/Interfaces/CategoryRepositoryInterface.php
@@ -1,18 +1,10 @@
-    <?php
+<?php
 
 namespace Modules\Categories\Domain\Interfaces;
 
 use Modules\Categories\Domain\Entities\Category;
-use Modules\Category\Domain\ValueObjects\CategoryId;
-use Modules\Category\Domain\ValueObjects\CategorySlug;
 
 interface CategoryRepositoryInterface
 {
     public function save(Category $category): void;
-
-    // public function getById(CategoryId $id): ?Category;
-
-    // public function existsBySlug(CategorySlug $slug, ?CategoryId $ignoreId = null): bool;
-
-    // public function delete(CategoryId $id): void;
 }
